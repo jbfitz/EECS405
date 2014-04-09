89be760aeb50da65140cbb5f2169c83c0137a3c1
James Fitzpatrick <james.fitzpatrick@case.edu>
2014-04-08 23:51:21 -0400

    Added Trie Structure for Top-k Algorithm

---
+++ TopKSearch/TopKSearch/TopKRangeSearch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TrieSpace;
+
+namespace TopKSearch
+{
+    class TopKRangeSearch
+    {
+        static Trie data = new Trie();
+
+        static void Main(string[] args)
+        {
+            // Construct Trie
+            try
+            {
+                //Console.WriteLine("Please input the filename you wish to read from:");
+                //String filename = Console.ReadLine();
+                ConstructTrie("The Prince.txt");
+
+                Console.WriteLine("What search do you want to look at?");
+                String querystring = Console.ReadLine();
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error Encountered: ");
+                Console.Error.WriteLine(e.Message);
+            }
+            Console.WriteLine("Press any button to exit...");
+            Console.ReadKey();
+
+        }
+
+
+        public static void ConstructTrie(String filename)
+        {
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                while (!sr.EndOfStream)
+                {
+                    String ln = sr.ReadLine();
+                    if (ln.Length > 0)
+                    {
+                        String[] words = ln.Split(' ');
+                        if (words.Length > 0)
+                        {
+                            data.AddStrings(words);
+                        }
+                    }
+                }
+                data.SetIds();
+                Console.WriteLine(data.root.max);
+                Console.WriteLine("Trie constructed!");
+            }
+
+        }
+    }
+}
---
+++ TopKSearch/TopKSearch/Trie.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrieSpace;
+
+
+
+namespace TrieSpace
+{
+    public class Trie
+    {
+        public TrieNode root;
+
+        public Trie()
+        {
+            root = new TrieNode('\0');
+        }
+
+        public void AddStrings(String[] strings)
+        {
+            if (strings.Length == 0)
+            {
+                Console.Error.WriteLine("Warning: No Strings were passed in. Exiting Trie Construction...");
+                return;
+            }
+
+            foreach (String s in strings)
+            {
+                if (s != "")
+                {
+                    root.AddString(s);
+                }
+            }
+        }
+
+        public void SetIds()
+        {
+            root.SetIds();
+        }
+
+        override public String ToString()
+        {
+            return root.ToString();
+        }
+
+    }
+
+    public class TrieNode
+    {
+        public Dictionary<char, TrieNode> children;
+        public char character { get; set; }
+        public int max { get; set; }
+        public int min { get; set; }
+        public int id;
+        public bool root { get; set; }
+        public bool endOfWord;
+
+
+        public TrieNode(char c)
+        {
+            character = c;
+            children = new Dictionary<char, TrieNode>();
+            endOfWord = false;
+        }
+
+
+        //Recursively adds the content of the string to the node as children. 
+        public void AddString(String s)
+        {
+            char firstChar = s[0];
+            AddChild(firstChar);
+            if (s.Substring(1) != "")
+            {
+                children[firstChar].AddString(s.Substring(1));
+            }
+            else
+            {
+                children[firstChar].endOfWord = true;
+            }
+        }
+
+        public void AddChild(char c)
+        {
+            if (!this.children.ContainsKey(c))
+            {
+                this.children.Add(c, new TrieNode(c));
+            }
+        }
+
+        public int SetIds()
+        {
+            return SetIds(1);
+        }
+
+        public int SetIds(int currentId)
+        {
+            this.min = currentId;
+            if (endOfWord)
+            {
+                this.id = currentId++;
+            }
+
+            if (children.Count > 0)
+            {
+                foreach (TrieNode child in children.Values)
+                {
+                    currentId = child.SetIds(currentId) + 1;
+                }
+                this.max = currentId - 1;
+            }
+            else
+            {
+                this.max = this.min;
+            }
+            return this.max;
+        }
+
+        public bool IsLeaf()
+        {
+            return endOfWord;
+        }
+
+        override public String ToString()
+        {
+            if (children.Count > 0)
+            {
+                String returnString = "";
+                foreach (TrieNode child in children.Values)
+                {
+                    returnString += this.character + child.ToString();
+                }
+
+                if (this.endOfWord)
+                {
+                    returnString += this.character;
+                }
+                return returnString;
+
+            }
+            else
+            {
+                return this.character.ToString();
+            }
+        }
+    }
+}
